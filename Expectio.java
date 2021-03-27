5423f56a90ae6c1e90d053605661184c1e15442f
abc <abc@example.com>
2021-03-27 20:21:13 +0530

    added

---
+++ Expectio.java
@@ -0,0 +1,46 @@
+
+public class Expectio 
+{
+	/*blic void fun(int age)
+	{
+		if(age<18)
+		{
+			throw  new ArithmeticException();
+			
+		}	
+		else
+		{
+			System.out.println("matured");
+			
+		}
+	}*/
+	
+	
+	
+	
+	
+	
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=0;
+		int c=0;
+		try
+		{
+			
+			c=a/b;
+				
+		}catch(Exception e) {
+			System.out.println("catch block executed");
+			e.printStackTrace();
+		}
+		finally
+		{
+			System.out.println("finally block");
+		}
+		
+		/*ectio e=new Expectio();
+		e.fun(20);*/
+
+}
+}
